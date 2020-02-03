2147dd3e087a388769bbde1050421abd5d98fe43
Fred Hohman <fred.hohman@gmail.com>
2020-02-03 15:21:47 -0500

    add research x practice table

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
 
 import Example from "./diagrams/svelte-example.svelte";
+import ResearchPracticeTable from "./diagrams/research-x-practice-table.svelte"
 
 // lazily initialize any diagram below the fold. E.G:
-const exampleTag = document.getElementById("svelte-example-dfigure");
-let example;
-exampleTag.addEventListener("ready", () => {
+
+// const exampleTag = document.getElementById("svelte-example-dfigure");
+// let example;
+// exampleTag.addEventListener("ready", () => {
 	// const target = exampleTag.querySelector("#svelte-example-target");
 	// example = new Example({ target });
+// });
+
+const researchPracticeTable = new ResearchPracticeTable({
+	target: document.getElementById('research-x-practice')
 });
+
