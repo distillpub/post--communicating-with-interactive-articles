e4330668bedc738645935821bbaacde7b0e8e067
Fred Hohman <fred.hohman@gmail.com>
2020-02-03 15:21:47 -0500

    add research x practice table

---
+++ src/diagrams/research-x-practice-table.svelte
@@ -0,0 +1,59 @@
+<style>
+  #research-x-practice-table {
+    overflow-x: scroll;
+    width: 100%;
+  }
+
+  #research-x-practice-table th {
+    white-space: nowrap;
+  }
+
+  #research-x-practice-table tbody th {
+    font-weight: initial;
+    border-bottom: 1px solid rgb(242, 242, 242);
+  }
+
+  #research-x-practice-table tbody tr:last-of-type th {
+    border-bottom: inherit;
+  }
+
+  /* #research-x-practice-table td,
+  #research-x-practice-table thead th {
+    text-align: center;
+  } */
+
+  #research-x-practice-table td {
+    border-color: rgb(242, 242, 242);
+  }
+
+  /* #research-x-practice-table td.highlight {
+    background-color: #f6f6f6;
+  } */
+</style>
+
+<table id="research-x-practice-table">
+  <thead>
+    <tr>
+      <th />
+      <th scope="col">Research</th>
+      <th scope="col">Practice</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Authoring</th>
+      <td>Next generation tooling</td>
+      <td>Evaluate in production setting, identify bugs</td>
+    </tr>
+    <tr>
+      <th scope="row">Designing</th>
+      <td>Developing theory, conducting laboratory studies</td>
+      <td>Evaluate specific design decisions in the wild, understand constraints</td>
+    </tr>
+    <tr>
+      <th scope="row">Publishing</th>
+      <td>Tools, guidelines, and best practices</td>
+      <td>Concrete examples for others to follow, available source code, accessible archives, DOI, branding</td>
+    </tr>
+  </tbody>
+</table>
