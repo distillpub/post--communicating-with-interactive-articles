36019fcc35b0738088d630f8b8385b3985b791d7
Fred Hohman <fred.hohman@gmail.com>
2020-03-02 15:22:53 -0500

    update bib with interactive article examples

--- src/diagrams/example-table.svelte
+++ src/diagrams/example-table.svelte
@@ -21,16 +21,21 @@
 
 <style>
     #example-table-wrapper {
-        height: 500px;
+        /* height: 500px; */
         background-color: var(--gray-bg);
         border: 1px solid var(--gray-border);
         border-radius: var(--border-radius);
         margin-bottom: 1em;
+        padding: 1.5em;
+        /* overflow: scroll */
     }
 </style>
 
 <div id="example-table-wrapper">
+    <div>Number of citations: {bibliography.length}</div>
+    <div>Number of article examples: {examples.length}</div>
+    <hr>
     {#each examples as example}
-        <div>{example.entryTags.title}</div>
+        <div style="text-transform: capitalize">{example.entryTags.title}<d-cite key={example.citationKey}></d-cite></div>
 	{/each}
 </div>
