1058f805f79fa480a82da9b95f16f2c2b0e21b7e
Fred Hohman <fred.hohman@gmail.com>
2020-03-03 16:12:37 -0500

    sort table

--- src/diagrams/example-table.svelte
+++ src/diagrams/example-table.svelte
@@ -2,6 +2,10 @@
     import bibtexParse from 'bibtex-parse-js';
     import bibString from '../../static/bibliography.bib';
 
+    let sortAsc = true;
+    let sortKey = "title"
+    let lastSortKey = ""
+
     let bibliography = bibtexParse.toJSON(bibString);
 
     // filter out all bibtex entries except interactive article examples
@@ -9,18 +13,36 @@
         return bibEntry.entryTags.howpublished === "web";
     });
 
-    // coerice tag string into array of strings
     examples.forEach(citation => {
-        if (citation.entryTags.tags) {
-            citation.entryTags.tags = citation.entryTags.tags.split(',')
-        }        
+        // coerice tag string into array of strings
+        citation.entryTags.tags = citation.entryTags.tags.split(',')
+
+        // give examples with no jounal string with space
+        if (!citation.entryTags.journal) {
+            citation.entryTags.journal = " "
+        }
     });
 
     function sort() {
-        // console.log('sort')
-        examples.sort((a,b) => (a.entryTags.year > b.entryTags.year) ? 1 : ((b.entryTags.year > a.entryTags.year) ? -1 : 0)); 
+        if (lastSortKey !== sortKey) {
+            sortAsc = true
+            examples.sort((a,b) => (a.entryTags[sortKey] > b.entryTags[sortKey]) ? 1 : ((b.entryTags[sortKey] > a.entryTags[sortKey]) ? -1 : 0)); 
+        } else {
+            if (sortAsc) {
+                sortAsc = false
+                examples.sort((a,b) => (a.entryTags[sortKey] > b.entryTags[sortKey]) ? -1 : ((b.entryTags[sortKey] > a.entryTags[sortKey]) ? 1 : 0));
+            } else {
+                sortAsc = true
+                examples.sort((a,b) => (a.entryTags[sortKey] > b.entryTags[sortKey]) ? 1 : ((b.entryTags[sortKey] > a.entryTags[sortKey]) ? -1 : 0)); 
+            }
+        }
         examples = examples
+        console.log(lastSortKey, sortKey, sortAsc)
     }
+
+    // initial sort on load
+    sort()
+
 </script>
 
 <style>
@@ -33,6 +55,10 @@
         padding: 1.5em;
         /* overflow: scroll */
     }
+
+    th:hover {
+        cursor: pointer;
+    }
 </style>
 
 <div>Number of citations: {bibliography.length}</div>
@@ -42,9 +68,9 @@
     <table id="research-x-practice-table">
         <thead>
             <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Publication</th>
-            <th scope="col" on:click={sort}>Year</th>
+            <th scope="col" on:click={() => {lastSortKey = sortKey; sortKey = "title"; sort()}}>Title</th>
+            <th scope="col" on:click={() => {lastSortKey = sortKey; sortKey = "journal"; sort()}}>Publication</th>
+            <th scope="col" on:click={() => {lastSortKey = sortKey; sortKey = "year"; sort()}}>Year</th>
             </tr>
         </thead>
         <tbody>
@@ -54,7 +80,8 @@
                     <a href={example.entryTags.url}>{example.entryTags.title}</a>
                     <!-- <d-cite key={example.citationKey}></d-cite> -->
                 </td>
-                <td>{example.entryTags.journal}</td>
+                <td>{example.entryTags.journal ? example.entryTags.journal : ""}</td>
+                <!-- <td>{example.entryTags.tags}</td> -->
                 <td>{example.entryTags.year}</td>
                 </tr>
             {/each}
