4b66694ccf61df6c06101b53bc8a1760d74c906f
Fred Hohman <fred.hohman@gmail.com>
2020-03-30 10:46:25 -0400

    add title component, add title to horse

---
+++ src/diagrams/title.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let titleText;
+    export let subtitleText;
+</script>
+
+<style>
+
+    #title {
+        font-weight: 700;
+        font-size: 20px;
+    }
+
+    #subtitle {
+        margin-bottom: 1em;
+        color: var(--gray);
+    }
+
+    /* div {
+        text-align: center
+    } */
+
+    img {
+        padding-right: 0.5em;
+    }
+
+</style>
+
+<div>
+    
+    <!-- <div style="display: flex"> -->
+        <!-- <img src="images/pointer.svg" alt="Pointer: interactive diagram."> -->
+        <div id="title">{titleText}</div>
+    <!-- </div> -->
+    <div id="subtitle">{subtitleText}</div>
+</div>
