cca35ac4675d4ae70e9f20a0eb1a56f0e9350f38
Fred Hohman <fred.hohman@gmail.com>
2020-03-30 11:50:34 -0400

    add titles to details figures

---
+++ src/diagrams/details-illustration.svelte
@@ -0,0 +1,23 @@
+<script>
+
+    import Title from "./title.svelte";
+
+</script>
+
+<style>
+
+    #placeholder {
+        height: 300px;
+        background-color: lightgray
+    }
+
+</style>
+
+<div>
+    <Title
+        titleText="[TK]."
+        subtitleText="[TK]."
+    />
+
+    <div id="placeholder"></div>
+</div>
