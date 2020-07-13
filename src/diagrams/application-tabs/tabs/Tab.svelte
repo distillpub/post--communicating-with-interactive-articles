d1589c04d5495865ed3bbb277bc3a1ceb234a653
Fred Hohman <fred.hohman@gmail.com>
2020-07-13 18:21:32 -0400

    responsive application tab

--- src/diagrams/application-tabs/tabs/Tab.svelte
+++ src/diagrams/application-tabs/tabs/Tab.svelte
@@ -20,6 +20,7 @@
 		cursor: pointer;
 		flex: 1;
 		border-bottom: 3px solid var(--gray-bg);
+		/* width: 25%; */
 		/* border-left: 1px solid var(--gray-border); */
 	}
 
@@ -30,6 +31,15 @@
 	.selected {
 		border-bottom: 3px solid var(--orange);
 		color: #333;
+	}
+
+	@media(max-width: 1000px) {
+
+		button {
+			padding: 1em 0.5em;
+			font-size: 0.85em;
+		}
+
 	}
 </style>
 
