4340f5bab1cecb503fea536e68dcb09dfea1e3a7
Fred Hohman <fred.hohman@gmail.com>
2020-08-28 17:25:34 -0400

    add 250px top and bottom observer margin

--- src/diagrams/intersection-observer.svelte
+++ src/diagrams/intersection-observer.svelte
@@ -2,8 +2,8 @@
   import { onMount } from 'svelte';
 
   export let once = false;
-  export let top = 0;
-  export let bottom = 0;
+  export let top = 250;
+  export let bottom = 250;
   export let left = 0;
   export let right = 0;
 
